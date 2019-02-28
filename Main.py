6d1e98ce7bc5c31418b967f54922a8cef8d629c1
Arthur Deruytter <art.deruytter@gmail.com>
2019-02-28 22:17:33 +0100

    niets aangepast

--- Main.py
+++ Main.py
@@ -1,11 +1,7 @@
 H = []  # Horizontale foto's
 V = []  # Verticale foto's
 #bestand = input("Whatever het bestand is dat we willen checken: ")
-<<<<<<< HEAD
-bestand = "c_memorable_moments.txt"
-=======
 bestand = "e_shiny_selfies.txt"
->>>>>>> master
 from InvoerVerwerker import leesLijn
 from UitvoerVerwerker import output
 #from Photos import max
