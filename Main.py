338b5bfef1a3d92e3b5103111cd13e3addc9ff59
Arno Vermote <mannefakker@gmail.com>
2019-02-28 21:00:15 +0100

    main WIP

--- Main.py
+++ Main.py
@@ -6,3 +6,14 @@ from InvoerVerwerker import leesLijn
 
 leesLijn(H, V, bestand)
 
+    # TODO: eerste geldige dia op True zetten
+if H[0][0] < 2:
+      # TODO: maak eerste dia H[0]
+    H[0][1]= True
+else:
+
+    V[0][1]= True
+    V[1][1]=True
+
+
+
