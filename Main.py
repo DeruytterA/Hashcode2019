e8133067e8cb9e7140768d5ee3ff519601b3dbba
Arno Vermote <mannefakker@gmail.com>
2019-02-28 21:36:14 +0100

    done?

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 H = []  # Horizontale foto's
 V = []  # Verticale foto's
 #bestand = input("Whatever het bestand is dat we willen checken: ")
-bestand = "a_example.txt"
+bestand = "b_lovely_landscapes.txt"
 from InvoerVerwerker import leesLijn
 from UitvoerVerwerker import output
 #from Photos import max
--- UitvoerVerwerker.py
+++ UitvoerVerwerker.py
@@ -1,4 +1,14 @@
 def output(V, H, eersteSlide):
     from Photos import Max
-    aantalSlides = len(H) + len(V)/2
-    print(Max(H, V, eersteSlide))
+    aantalSlides = len(H) + len(V) // 2
+    resultaat = open("resultaat.txt", "w+")
+    resultaat.write(str(aantalSlides) + "\n")
+    resultaat.write(str(eersteSlide[0][0]) + "\n")
+    for teller in range(1, aantalSlides):
+        maxOutput = Max(H, V, eersteSlide)
+        H = maxOutput[0]
+        V = maxOutput[1]
+        if len(maxOutput[2]) == 2:
+            resultaat.write(str(maxOutput[2][0][0]) + " " + str(maxOutput[2][1][0]) + "\n")
+        else:
+            resultaat.write(str(maxOutput[2][0][0]) + "\n")
