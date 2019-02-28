79c23327159cc127088e252a4e404c5747121c7b
Arno Vermote <mannefakker@gmail.com>
2019-02-28 22:06:21 +0100

    Main bugfix

--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
 H = []  # Horizontale foto's
 V = []  # Verticale foto's
 #bestand = input("Whatever het bestand is dat we willen checken: ")
-bestand = "b_lovely_landscapes.txt"
+bestand = "e_shiny_selfies.txt"
 from InvoerVerwerker import leesLijn
 from UitvoerVerwerker import output
 #from Photos import max
 
 leesLijn(H, V, bestand)
-eersteSlide = [H[0]]
-eersteSlide[0][1] = True
+if not H:
+    V[0][1] = True
+    V[1][1] = True
+    eersteSlide = [V[0], V[1]]
+
+else:
+    H[0][1] = True
+    eersteSlide = [H[0]]
+
 
 output(V, H, eersteSlide)
 
