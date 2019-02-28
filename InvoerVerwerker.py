a4dbe0ce81150a7afacd2da0d0d8802489478b8a
Arno Vermote <mannefakker@gmail.com>
2019-02-28 20:17:32 +0100

    Begin van Main FINAL van Invoerverwerker

---
+++ InvoerVerwerker.py
@@ -0,0 +1,23 @@
+H = []
+V = []
+def leesLijn(H, V, bestand):
+    with open(bestand) as f:
+        aantalFotos = int(f.readline())
+        for fotoID in range(aantalFotos):
+            lijn = f.readline()
+            lijnlijst = (lijn[:-1].split(" "))
+            lijnlijst[1] = False
+            lijnlijst = [fotoID] + lijnlijst
+            if lijnlijst[1] == "H":
+                H.append([lijnlijst[0]] + lijnlijst[2:])
+            else:
+                V.append([lijnlijst[0]] + lijnlijst[2:])
+
+
+leesLijn(H, V, "a_example.txt")
+
+
+
+
+
+
